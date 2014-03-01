a6e675140f9076e550930acf8ed0128f73319531
Bastien Dejean <nihilhill@gmail.com>
2014-03-01 22:06:48 +0100

    Update

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,54 @@
+#! /bin/sh
+
+[ -d "$HOME"/.bin ] || git clone -b macosx git@github.com:baskerville/bin.git "$HOME"/.bin
+[ -d "$HOME"/.dotfiles ] || git clone -b macosx git@github.com:baskerville/dotfiles.git "$HOME"/.dotfiles
+
+brew --version > /dev/null 2>&1 || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+echo "Press *return* to continue"
+read
+
+brew update
+brew upgrade
+# brew install osxfuse ext2fuse
+brew install wget tmux openssl
+brew install vim ctags rename rlwrap
+brew install lua go ghc
+brew install node mongodb
+brew install mpop msmtp mpack
+brew install --HEAD --with-s-lang mutt
+brew install --HEAD slrn
+brew install base64 hunspell
+brew install potrace sdl
+brew install youtube-dl mediainfo 
+brew install imagemagick ffmpeg flac
+brew install aria2 mp4box pngnq
+brew install detox duff catdoc ranger
+brew install surfraw w3m dict wordnet
+brew install tree gifsicle libicns
+# brew install ghostscript
+# brew install pdfgrep
+
+brew install python python3 ruby
+pip3 install numpy colormath misaka Jinja2
+pip install docutils PyPDF2
+gem install chronic gpgme sup
+
+brew tap mpv-player/mpv
+brew install --HEAD mpv-player/mpv/libass-ct
+brew install mpv
+
+brew tap phinze/cask
+brew install brew-cask
+# brew cask install chromium
+brew cask install secrets keyremap4macbook quicksilver
+brew cask install shiftit burn gimp
+# brew cask install xquartz
+# brew cask install inkscape audacity mactex
+
+brew tap caskroom/fonts
+brew cask install font-dejavu font-fira-sans
+
+brew tap baskerville/formulae
+brew install z-go paps-bskv deheader mldonkey-bskv lhasa unlzx ppsei
+brew install --HEAD google-translate-cli spdf
---
+++ bootstrap/settings.sh
@@ -0,0 +1,115 @@
+#! /bin/sh
+
+# Set the timezone; see `systemsetup -listtimezones` for possible values
+# systemsetup -settimezone Europe/Paris
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Enable subpixel font rendering on non-Apple LCDs
+defaults write -g AppleFontSmoothing -int 2
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write -g com.apple.mouse.tapBehavior -int 1
+defaults write -g com.apple.mouse.tapBehavior -int 1
+
+# Disable “natural” (Lion-style) scrolling
+defaults write -g com.apple.swipescrolldirection -bool false
+
+# Disable iOS style long key presses
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write -g AppleKeyboardUIMode -int 3
+
+# Finder: show all filename extensions
+defaults write -g AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Add a context menu item for showing the Web Inspector in web views
+defaults write -g WebKitDeveloperExtras -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Increase window resize speed for Cocoa applications
+# defaults write -g NSWindowResizeTime -float 0.2
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Expand save panel by default
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expand print panel by default
+defaults write -g PMPrintingExpandedStateForPrint -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Display ASCII control characters using caret notation in standard text views
+defaults write -g NSTextShowsControlCharacters -bool true
+
+# Set Help Viewer windows to non-floating mode
+defaults write com.apple.helpviewer DevMode -bool true
+
+# Menu bar: disable transparency
+defaults write -g AppleEnableMenuBarTransparency -bool false
+
+# Menu bar: show remaining battery time
+# defaults write com.apple.menuextra.battery ShowTime -string "YES"
+
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+ 
+# Disable the Ping sidebar in iTunes
+defaults write com.apple.iTunes disablePingSidebar -bool true
+ 
+# Disable all the other Ping stuff in iTunes
+defaults write com.apple.iTunes disablePing -bool true
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion"
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Remove the auto-hiding Dock delay
+# defaults write com.apple.dock autohide-delay -float 0
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Disable window animations and Get Info animations in Finder
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Don't animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Disable opening and closing window animations
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
